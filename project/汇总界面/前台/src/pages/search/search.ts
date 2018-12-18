530714cec7c6de6b043546b48778772189718a11
yinyujia <1373722811@qq.com>
2018-12-18 17:03:42 +0800

    修改了搜索页

--- project/汇总界面/前台/src/pages/search/search.ts
+++ project/汇总界面/前台/src/pages/search/search.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 /**
  * Generated class for the SearchPage page.
  *
@@ -14,17 +14,60 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'search.html',
 })
 export class SearchPage {
+  // itemsfood=Array();
+  // arrfood=["city"];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+   constructor(public navCtrl: NavController, public navParams: NavParams, public http:HttpClient) {
+  //   for(var i=0;i<this.arrfood.length;i++){
+  //     this.http.get('http://192.168.142.144:8080/'+this.arrfood[i],{
+  //       headers:new HttpHeaders({}),
+  //       }).subscribe((data)=>{
+  //         this.itemsfood.push(data);
+  //       });
+  //   }
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
+    
   }
+
+  // myInput:string;
+  // objfood=[];
+  // output(){
+  //   if(this.myInput){
+  //     var reg =new RegExp(this.myInput,"g");
+  //     for(var i=0;i<this.itemsfood.length;i++){
+  //       for(var j=0;j<this.itemsfood[i].length;j++){
+  //         if(reg.test(this.itemsfood[i][j].name)){
+  //           this.objfood.push(this.itemsfood[i][j]);
+  //         }
+  //       }
+  //     }
+  //   }
+  
+  // getinformation(){
+  //   this.objfood=[];
+  // }
   pushSearch(){
     this.navCtrl.pop();
   }
   close(){
     this.navCtrl.pop();
   }
+
+  todos:'';
+  todolist=[];
+  add(e){
+    var obj={    
+      todos:this.todos,
+    }
+  if (e.keyCode==13) {
+      this.todolist.push(obj);  
+      this.todos='';    
+  }
+  }
+  del(){
+    this.todolist.splice(0,this.todolist.length);
+  }
+
 }
