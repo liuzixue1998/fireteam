8eb67475a6a206bc8b932610e4eac0614776b691
yinyujia <1373722811@qq.com>
2018-12-19 16:39:18 +0800

    一些提交

---
+++ project/汇总界面/后台/adminLTE-Node/config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    secret: '123456'
+
+}
---
+++ project/汇总界面/后台/adminLTE-Node/routes/users.js
@@ -0,0 +1,43 @@
+var express = require('express');
+  var router = express.Router();
+  var db = require("../config/db");
+  /* GET users listing. */
+  router.get('/', function (req, res, next) {
+	res.send('respond with a resource');
+  });
+  router.get('/list', function (req, res, next) {
+	db.query("select * from admin_user", function (err, rows) {
+	  if (err) {
+		res.render("list", {
+		  title: "用户列表",
+		  datas: []
+		});
+	  } else {
+		res.render("list", {
+		  title: "用户列表",
+		  users: rows,
+		  cookie: req.cookies
+		});
+	  }
+	});
+  });
+  /**
+   * 返回用户列表
+   */
+  router.get('/listjson', function (req, res, next) {
+	db.query("select * from admin_user", function (err, rows) {
+	  if (err) {
+		res.json({
+		  code: 1,
+		  data: []
+		})
+	  } else {
+		res.json({
+		  code: 0,
+		  data: rows,
+		  cookie: req.cookies
+		})
+	  }
+	});
+  });
+  module.exports = router;
