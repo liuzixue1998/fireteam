9b71042b849c354a6db0e1b714774fcf1e8dde8f
yinyujia <1373722811@qq.com>
2018-12-24 17:25:02 +0800

    修改了注册、登录页面

--- project/汇总界面/前台/src/pages/signin/signin.ts
+++ project/汇总界面/前台/src/pages/signin/signin.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, App } from 'ionic-angular';
 import { ContactPage } from '../contact/contact';
 import { SignupPage } from '../signup/signup';
-import { MyPage } from '../my/my';
+//import { MyPage } from '../my/my';
 import { AccountPage } from '../account/account';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { TabsPage } from '../tabs/tabs';
 
 /**
  * Generated class for the SigninPage page.
@@ -18,8 +20,32 @@ import { AccountPage } from '../account/account';
   templateUrl: 'signin.html',
 })
 export class SigninPage {
+  private headers=new HttpHeaders({'Content-Type':'application/x-www-form-urlencoded'} )
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  users;
+  usersName = [];
+  usersPassword = [];
+  user;
+  code;
+  name:string;
+  password:string;
+  correct;
+  key;
+  man;
+  tishi;
+  constructor(public app:App,public alertCtrl:AlertController,public http:HttpClient,public navCtrl: NavController, public navParams: NavParams) {
+    this.http.get('http://192.168.142.144:8080/'+'users',{
+      headers:new HttpHeaders({
+      }),
+    }).subscribe((data) => { // 监听
+      this.users = data;
+      //console.log(this.users);
+      for(var i = 0;i<this.users.length;i++){
+        this.usersName[i] = data[i].name;
+        this.usersPassword[i] = data[i].password;
+      }
+      console.log(this.usersName,this.usersPassword);
+    });
   }
 ret(){
   this.navCtrl.push(AccountPage);
@@ -27,11 +53,28 @@ ret(){
 signup(){
   this.navCtrl.push(SignupPage);
 }
-myy(){
-  this.navCtrl.push(MyPage);
-}
+// myy(){
+//   //this.navCtrl.push(MyPage);
+// }
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SigninPage');
+    //console.log('ionViewDidLoad SigninPage');
   }
-
+  doPost(){
+    for(var i in this.usersName){
+      if(this.usersName[i] == this.name){
+        this.code = this.usersPassword[i];
+        //console.log(this.code)
+      }
+    }
+  
+    if(this.password == this.code && this.code){
+      //console.log(this.password)
+      this.navCtrl.push(TabsPage, this.user);
+      this.key = localStorage.setItem('key','ok');
+      this.man = localStorage.setItem('man',this.man);
+    }else{
+      this.tishi='用户名或密码错误';
+    }
+  }
 }
+
--- project/汇总界面/前台/src/pages/signup/signup.ts
+++ project/汇总界面/前台/src/pages/signup/signup.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App, AlertController } from 'ionic-angular';
 import { SigninPage } from '../signin/signin';
+import { TabsPage } from '../tabs/tabs';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 /**
  * Generated class for the SignupPage page.
@@ -15,14 +17,58 @@ import { SigninPage } from '../signin/signin';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  private headers=new HttpHeaders({'Content-Type':'application/x-www-form-urlencoded'} )
+  name;
+  password;
+  
+   logForm() {
+    console.log(this.name);
+    console.log(this.password);
+    this.http.post('http://192.168.142.144:8080/'+'users',{
+      "name":this.name,
+      "password":this.password,
+    },{
+        headers:this.headers,
+      }).subscribe((data)=>{
+      console.log(data);
+        //跳转到tabs页并将用户tel传给tabs
+        // this.app.getRootNavs()[0].setRoot(TabsPage,{username:this.tel});
+        this.navCtrl.push(SigninPage);
+        console.log(this.name);
+  
+      // -1 用户名为空 -2 密码为空 0 成功 1 用户名重复
+      // switch(data){
+      //   case -1:
+      //       this.presentPrompt('用户名不能为空');
+      //       break;
+        
+      //   case -2:
+      //       this.presentPrompt('密码不能为空');
+      //       break;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+      //   case 1:
+      //       this.presentPrompt('用户名重复');
+      //       break;
+      //   case 0:
+      //       this.goHome();
+      //       break;
+      // }
+    })
+
+}
+
+  constructor(public app:App,public alertCtrl:AlertController,public http:HttpClient,public navCtrl: NavController, public navParams: NavParams) {
   }
 ret(){
   this.navCtrl.push(SigninPage);
 }
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
+    //console.log('ionViewDidLoad SignupPage');
   }
-
+  goTabs(){
+    this.navCtrl.push(TabsPage);
+  }
+  goLogin(){
+    this.navCtrl.push(SigninPage);
+  }
 }
